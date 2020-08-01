81056bc54e750ec83cbdb3241d8efe6b09f89ca4
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2020-08-01 13:17:40 +0900

    토큰 분리

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
 import os
+from configs import datas
 
 bot = commands.Bot(command_prefix='g!')
+with open(datas.TOKEN_FILE, 'r', encoding='utf-8') as f:
+    token = f.read()
 
 for ext in list(filter(lambda x: x.endswith('.py'), os.listdir('./cogs'))):
     bot.load_extension('cogs.' + os.path.splitext(ext)[0])
 
-bot.run('NjAyMDM1MDcyMTQwNTc0NzIw.XTK-2g.7lEXGOK4wWFQyFpqfVt2Qh4RIWE')
+bot.run(token)
---
+++ configs/datas.py
@@ -0,0 +1,1 @@
+TOKEN_FILE = 'C://gmbt/token.txt'
